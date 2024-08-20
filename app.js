6259e28892b18265ec2dd0ed4405d4e878dc3097
ta-mj <kiatae0722@naver.com>
2024-08-20 22:34:12 +0900

    Feat: "이미지 저장 및 url 반환 기능" client가 전송한 이미지를 s3 스토리지에 저장 후 저장된 이미지의 url을 반환합니다. Resolves: #19 Ref: Related to:

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import 'dotenv/config';
 import errorHandler from './middlewares/errorHandler.js';
 
 import groupController from './controllers/groupController.js';
+import imageController from './controllers/imageController.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/groups', groupController);
+app.use('/api/image', imageController);
 
 app.use(errorHandler);
 
