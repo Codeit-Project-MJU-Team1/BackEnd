3b018c13de3f8a83e833cde91f7fe1ea637bec5b
ta-mj <kiatae0722@naver.com>
2024-08-21 23:16:45 +0900

    Feat : "서버 CORS 설정" 현재는 프론트 로컬 환경에서 테스트 하기 위해 모든 주소에 대해 허용해두었지만, 추후 프론트 배포시 CORS 설정 수정 예정

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import errorHandler from './middlewares/errorHandler.js';
 
@@ -7,6 +8,7 @@ import imageController from './controllers/imageController.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/groups', groupController);
