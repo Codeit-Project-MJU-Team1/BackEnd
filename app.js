abab0fa33664dbc5fc3bd0cf6dd4806d877bd363
ta-mj <kiatae0722@naver.com>
2024-08-22 16:30:12 +0900

    Feat : "Test 브랜치 cors 설정"

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import errorHandler from './middlewares/errorHandler.js';
 
 import groupController from './controllers/groupController.js';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/groups', groupController);
